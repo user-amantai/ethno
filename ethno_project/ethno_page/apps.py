f830a709c138125d53f3565d914a5ebc72ebeef3
user-amantai <127530754+user-amantai@users.noreply.github.com>
2023-05-09 19:14:22 +0600

    Delete ethno_project directory

--- ethno_project/ethno_page/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EthnoPageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ethno_page'
--- ethno_project/ethno_page/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from ethno_page.views import *
-
-urlpatterns = [
-    path('', home),
-    path('test/', test),
-]
